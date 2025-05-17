afd1427cd3ae0ecdcdf9c333ccf50ce5c168b8c2
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-05-17 20:31:30 +0530

    Update: context setup

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const CartContext = createContext()
+
+const CartProvider = ({ children }) => {
+    return (
+        <CartContext.Provider value={{}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export { CartProvider, CartContext }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { CartProvider } from "../contexts/CartContext";
 
 export default function App({ Component, pageProps }) {
-  return <>
-    <Navbar />
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <CartProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </CartProvider>
+    </>
+  )
 }
